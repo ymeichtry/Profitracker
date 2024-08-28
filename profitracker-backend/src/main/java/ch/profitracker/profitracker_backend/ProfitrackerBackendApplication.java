663cf51374f9c25cb9a489db082d3be35130fca4
Yanis Meichtry <yanis@meichtry.dev>
2024-08-28 20:04:38 +0200

    added SpringBoot backend

---
+++ profitracker-backend/src/main/java/ch/profitracker/profitracker_backend/ProfitrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.profitracker.profitracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfitrackerBackendApplication.class, args);
+	}
+
+}
