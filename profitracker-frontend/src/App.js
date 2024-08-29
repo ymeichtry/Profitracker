6be9de13a7ba475e2271197934346a6c11d1775d
CriminalChicken <y.meichtry@gmx.ch>
2024-08-29 14:24:25 +0200

    added wouter for the route

--- profitracker-frontend/src/App.js
+++ profitracker-frontend/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
+import { Link, Route, Switch } from "wouter";
+import LoginPage from './components/login/LoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World! Hello Profitracker!
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Link href="/users/1">Profile</Link>
+
+    <Route path="/about">About Us</Route>
+
+    <Switch>
+      <Route path="/login" component={LoginPage} />
+
+      <Route path="/users/:name">
+        {(params) => <>Hello, {params.name}!</>}
+      </Route>
+
+      <Route>404: No such page!</Route>
+    </Switch>
+  </>
+);
 
 export default App;
